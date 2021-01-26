95ce4caa02cfe9d5a9f92e7552c9dfd6da714c54
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-01-26 14:42:08 -0500

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -50,9 +50,9 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-   // Set Motor to low initially
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
+  // Set Motor to low initially
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   digitalWrite(enA, LOW);
@@ -70,80 +70,80 @@ void loop() {
     // Serial.println(data.joyposX);
     //Serial.print("Y : ");
     // Serial.println(data.joyposY);
-   // Serial.print("potValue : ");
+    // Serial.print("potValue : ");
     //Serial.println(data.potValue);
     delay(10);
+
+
+    // X-axis used for forward and backward control
+    if (data.joyposX > 600) {
+      // Set Motor A backward
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      // Set Motor B backward
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
+      motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
+    }
+    else if (data.joyposX < 400) {
+      // Set Motor A forward
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      // Set Motor B forward
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+      // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
+      motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
+    }
+    // If joystick stays in middle the motors are not moving
+    else {
+      motorSpeedA = 0;
+      motorSpeedB = 0;
+    }
+
+
+    // Y-axis used for left and right control
+    //this is left
+    if (data.joyposY < 400) {
+
+      // Set Motor A forward
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      // Set Motor B backward
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+
+      // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
+      motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
+    }
+
+
+    if (data.joyposY > 600) {
+
+      // Set Motor A forward
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      // Set Motor B backward
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+
+      // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
+      motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
+    }
+    // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
+    if (motorSpeedA < 70) {
+      motorSpeedA = 0;
+    }
+    if (motorSpeedB < 70) {
+      motorSpeedB = 0;
+    }
+    analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
+    analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+
   }
-
-  // X-axis used for forward and backward control
-  if (data.joyposX > 600) {
-    // Set Motor A backward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    // Set Motor B backward
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-    // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
-    motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
-    motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
-  }
-  else if (data.joyposX < 400) {
-    // Set Motor A forward
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    // Set Motor B forward
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-    // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
-    motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
-    motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
-  }
-  // If joystick stays in middle the motors are not moving
-  else {
-    motorSpeedA = 0;
-    motorSpeedB = 0;
-  }
-
-
-  // Y-axis used for left and right control
-  //this is left
-  if (data.joyposY < 400) {
-
-    // Set Motor A forward
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    // Set Motor B backward
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-
-    // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
-    motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
-    motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
-  }
-
-
-  if (data.joyposY > 600) {
-
-    // Set Motor A forward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    // Set Motor B backward
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
-    // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
-    motorSpeedA = map(data.potValue, 1023, 0, 0, 255);
-    motorSpeedB = map(data.potValue, 1023, 0, 0, 255);
-  }
-  // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-  if (motorSpeedA < 70) {
-    motorSpeedA = 0;
-  }
-  if (motorSpeedB < 70) {
-    motorSpeedB = 0;
-  }
-  analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
-  analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
-
-
 }
